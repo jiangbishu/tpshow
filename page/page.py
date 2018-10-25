11a1d3ac7d70d4dce2151bc7335dfb5e51bfc12b
jiangbishu <18122718592@163.com>
2018-10-25 11:52:09 +0800

    第一次提交

---
+++ page/page.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+from page.page_home import HomePageAction
+
+
+class Page:
+
+    def inithomepage(self):
+        return HomePageAction()
---
+++ page/page_home.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+
+
+class HomePageAction():
+
+    # 给 home 模型定义了一个动作，可以实现自动进入首页的操作
+    def auto_enter_home(self):
+        print( "欢迎来到首页" )
